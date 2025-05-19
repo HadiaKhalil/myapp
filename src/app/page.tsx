74306982e131c1bb89b95df9ffc3b958af1b3c37
Wajeeha Arain <arainwajeeha715@gmail.com>
2025-05-19 10:58:54 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,38 @@ const page = () => {
     </div>
    </div>
    
-   
-    <div>mra projct hai bhn</div>
+       <div>mra projct hai bhn</div>
+             <p className='bg-white text-black'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus, facilis dolore impedit veniam maiores! Natus perspiciatis quis dolore sint repellendus saepe adipisci at quasi, accusamus quibusdam quaerat sunt qui?
+      </p>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <p className='bg-white text-black'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus, facilis dolore impedit veniam maiores! Natus perspiciatis quis dolore sint repellendus saepe adipisci at quasi, accusamus quibusdam quaerat sunt qui?
+      </p>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+
    </>
 
 
