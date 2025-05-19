8231c8fa0c979b0b3c9460f6cacfa4fc8b164167
Wajeeha Arain <arainwajeeha715@gmail.com>
2025-05-19 10:32:08 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page.....</div>
+   <>
+   <div>
+    <div>
+      <p className='bg-white text-black'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus, facilis dolore impedit veniam maiores! Natus perspiciatis quis dolore sint repellendus saepe adipisci at quasi, accusamus quibusdam quaerat sunt qui?
+      </p>
+    </div>
+   </div>
+   
+   
+   </>
   )
 }
 
