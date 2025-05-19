98fbcce852f2b8ae40dde9149f41d369cc333596
Wajeeha Arain <arainwajeeha715@gmail.com>
2025-05-19 10:52:20 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 
 const page = () => {
   return (
+<<<<<<< HEAD
    <>
    <div>
     <div>
       <p className='bg-white text-black'>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem ducimus, facilis dolore impedit veniam maiores! Natus perspiciatis quis dolore sint repellendus saepe adipisci at quasi, accusamus quibusdam quaerat sunt qui?
       </p>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+      <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
+            <div>
+        <p  className='bg-white text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, exercitationem. Et perspiciatis itaque obcaecati? Porro doloremque velit corporis at autem, laudantium quasi fuga hic, accusantium debitis reiciendis deleniti dolore vero?</p>
+      </div>
     </div>
    </div>
    
    
    </>
+=======
+    <div>mra projct hai bhn</div>
+>>>>>>> f14ce5c047107e364e1a7e6c745afdb0ce95a121
   )
 }
 
